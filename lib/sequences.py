10e163b2ea4bf6486bd2f20f0afd84152d792a6e
Abdinasir Dahir <abdinasir.dahir@moringaschool.com>
2024-08-29 15:46:12 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    result = [0] * length  
+    if length > 0:
+        result[0] = 0 
+    if length > 1:
+        result[1] = 1  
+    for i in range(2, length):
+        result[i] = result[i - 1] + result[i - 2]
+    print(result)
